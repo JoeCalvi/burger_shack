6765889bfc42125b74673c75769e260231b47e24
JoeCalvi <joecalvi@me.com>
2023-02-13 15:54:32 -0700

    starting project

---
+++ server/db/FakeDb.js
@@ -0,0 +1,18 @@
+
+class FakeDb {
+    burgers = [
+        {
+            id: '123312hkl23k',
+            name: 'The Classic',
+            toppings: 'lettuce, tomato, onion, ketchup, mustard, and mayo'
+        },
+        {
+            id: '162312hkl23g',
+            name: 'La Royale With Cheese',
+            toppings: '1/2lb patty, with the works'
+        }
+    ]
+}
+
+
+export const fakeDb = new FakeDb()
